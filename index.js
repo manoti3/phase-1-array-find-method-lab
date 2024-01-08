c25e02bbfdafea35b35e5532e0e933f83eb4c199
manoti3 <hillarymanoti0@gmail.com>
2024-01-08 16:08:03 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    
+];
+function superbowlWin(record){
+    let result =record.find(winner =>winner.result === "W")
+
+    if(result){
+        return result.year;
+    }
+    else{
+        return undefined
+    }
+
+}
+console.log(superbowlWin(record));
